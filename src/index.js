9f3a690da9489c182f278d7185bde393db746397
1527545535 <104073448+1527545535@users.noreply.github.com>
2024-07-03 11:36:11 +0800

    Update index.js
    修改域名

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.xiaojie.chat": dockerHub,
+  "quay.xiaojie.chat": "https://quay.io",
+  "gcr.xiaojie.chat": "https://gcr.io",
+  "k8s-gcr.xiaojie.chat": "https://k8s.gcr.io",
+  "k8s.xiaojie.chat": "https://registry.k8s.io",
+  "ghcr.xiaojie.chat": "https://ghcr.io",
+  "cloudsmith.xiaojie.chat": "https://docker.cloudsmith.io",
+  "ecr.xiaojie.chat": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
